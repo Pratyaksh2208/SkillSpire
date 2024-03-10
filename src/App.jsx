80f0f2390e311e68fcd79c4bbce76edd8bc99712
Puneet Yadav <rockstarpuneet@Puneets-MacBook-Air.local>
2024-03-10 12:50:08 +0530

    third commit p

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Home from "./Pages/Home";
+import Home from "./pages/Home";
+import Login from "./pages/Login.jsx";
+import Navbar from "./components/common/Navbar";
 
 function App() {
   return (
       <div className="w-screen min-h-screen bg-richblack-900 flex flex-col font-inter">
+        <Navbar/>
         <Routes>
           <Route path="/" element={<Home/> }/>
+          <Route path="/login" element={<Login/> }/>
         </Routes>
       </div>
   );
