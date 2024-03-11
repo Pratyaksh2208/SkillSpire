874f19523b7f309ea73585c0778dd326132d6ed9
Puneet Yadav <rockstarpuneet@Puneets-MacBook-Air.local>
2024-03-11 11:31:01 +0530

    login3

--- src/reducer/index.js
+++ src/reducer/index.js
@@ -1,12 +1,12 @@
-// // import { combineReducer } from "@reduxjs/toolkit";
-// import authReducer from "../slices/authReducer";
-// // import profileReducer from "../slices/profileSlice";
-// // import cartReducer from "../slices/cartSlice";
+import { combineReducer } from "@reduxjs/toolkit";
+import authReducer from "../slices/authReducer";
+import profileReducer from "../slices/profileSlice";
+import cartReducer from "../slices/cartSlice";
 
-// const rootReducer = combineReducer({
-//    auth: authReducer,
-// //    profile: profileReducer,
-// //    cart: cartReducer,
-// })
+const rootReducer = combineReducer({
+   auth: authReducer,
+   profile: profileReducer,
+   cart: cartReducer,
+})
 
-// export default rootReducer
+export default rootReducer
