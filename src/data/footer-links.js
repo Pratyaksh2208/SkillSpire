4b9fbdeb08c82997acddb7615eeef82fb4013314
Puneet Yadav <rockstarpuneet@Puneets-MacBook-Air.local>
2024-04-14 14:38:24 +0530

    last

--- src/data/footer-links.js
+++ src/data/footer-links.js
@@ -6,15 +6,7 @@ export const FooterLink2 = [
       { title: "Cloud Computing", link: "/cloud-computing" },
       { title: "Code Foundations", link: "/code-foundations" },
       { title: "Computer Science", link: "/computer-science" },
-      { title: "Cybersecurity", link: "/cybersecurity" },
-      { title: "Data Analytics", link: "/data-analytics" },
-      { title: "Data Science", link: "/data-science" },
-      { title: "Data Visualization", link: "/data-visualization" },
-      { title: "Developer Tools", link: "/developer-tools" },
-      { title: "DevOps", link: "/devops" },
-      { title: "Game Development", link: "/game-development" },
-      { title: "IT", link: "/it" },
-      { title: "Machine Learning", link: "/machine-learning" },
+ 
       { title: "Math", link: "/math" },
       { title: "Mobile Development", link: "/mobile-development" },
       { title: "Web Design", link: "/web-design" },
@@ -33,9 +25,7 @@ export const FooterLink2 = [
       { title: "JavaScript", link: "/javascript" },
       
       { title: "PHP", link: "/php" },
-      { title: "Python", link: "/python" },
-      { title: "R", link: "/r" },
-      
+     
       { title: "SQL", link: "/sql" },
       { title: "Swift", link: "/swift" },
     ],
@@ -46,9 +36,8 @@ export const FooterLink2 = [
         {title: "Career paths", link: "/career-paths"},
         {title: "Career services", link: "/career-services"},
         {title: "Interview prep", link: "/interview-prep"},
-        {title: "Professional certification", link: "/professional-certification"},
-        {title: "-", link: "/hi"},
-        {title: "Full Catalog", link: "/full-catalog"},
+      
+
         {title: "Beta Content", link: "/beta-content"}
     ]
   }
